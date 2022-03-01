1dc76df12cbd128a65102f0f4b60bba9b03ce747
kosuke-shibata <sittake1517@gmail.com>
2022-03-01 21:25:02 +0900

    usersテーブルのエンティティの編集

---
+++ database/migrations/2022_03_01_105711_add_user_info_column_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserInfoColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('introduce', 500);
+            $table->text('portfolio')->nullable();
+            $table->text('sns')->nullable();
+            $table->text('image_path')->nullable();
+            $table->string('credit', 255)->unique();
+            $table->string('password', 20)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
