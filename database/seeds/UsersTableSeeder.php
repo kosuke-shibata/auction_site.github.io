5647f598a808f1428c5eeab923144a30e9aac96a
kosuke-shibata <sittake1517@gmail.com>
2022-03-31 14:32:54 +0900

    userstableのクレジット情報の変更

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
             'id' => 1,
             'name' => 'しばたこうすけ',
             'introduce' => '初めまして。よろしくお願いします。',
-            'credit' => '123456789',
+            'credit' => '123456789012',
             'email' => 's.kosuke1517@gmail.com',
             'password' => '$2y$10$IAW6v6nxoaqG91IFck12i.UlLAQ2fTEPIS3C8OEr34ajrKtmxfw0K',
         ]);
@@ -24,7 +24,7 @@ class UsersTableSeeder extends Seeder
             'id' => 2,
             'name' => '山田太郎',
             'introduce' => '僕は山田太郎だ。男だ。22歳だ。',
-            'credit' => '987654321',
+            'credit' => '987654321098',
             'email' => 'yamada1111@gmail.com',
             'password' => '$2y$10$khqFRbuIpa6gYCfAQ/ygDusN0.MhjiiVX.RIi4YPsWdr8XHNj4ZqC',
         ]);
